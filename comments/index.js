c227c583a407df955b62ce30eb9c2df1cedb5352
neekyo <codebyns@gmail.com>
2024-11-14 15:20:02 -0500

    4 years later added all Dockerfiles & kubernetes config/deployments/services (:

--- comments/index.js
+++ comments/index.js
@@ -24,14 +24,14 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 	commentsByPostId[req.params.id] = comments;
 
-	await axios.post('http://localhost:4005/events', {
+	await axios.post('http://event-bus-srv:4005/events', {
 		type: 'CommentCreated',
 		data: {
 			id: commentId,
 			content,
 			postId: req.params.id,
-			status: 'pending'
-		}
+			status: 'pending',
+		},
 	});
 
 	res.status(201).send(comments);
@@ -51,14 +51,14 @@ app.post('/events', async (req, res) => {
 		});
 		comment.status = status;
 
-		await axios.post('http://localhost:4005/events', {
+		await axios.post('http://event-bus-srv:4005/events', {
 			type: 'CommentUpdated',
 			data: {
 				id,
 				status,
 				postId,
-				content
-			}
+				content,
+			},
 		});
 	}
 
--- posts/index.js
+++ posts/index.js
@@ -21,15 +21,15 @@ app.post('/posts', async (req, res) => {
 
 		posts[id] = {
 			id,
-			title
+			title,
 		};
 
-		await axios.post('http://localhost:4005/events', {
+		await axios.post('http://event-bus-srv:4005/events', {
 			type: 'PostCreated',
 			data: {
 				id,
-				title
-			}
+				title,
+			},
 		});
 
 		res.status(201).send(posts[id]);
